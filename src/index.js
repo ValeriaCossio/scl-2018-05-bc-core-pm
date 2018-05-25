04ba36a73eba0fa4d70a58b4bafd8e1fd24f1d97
Valeria Cossio <valeriacossio5@gmail.com>
2018-05-25 18:17:31 -0400

    html y css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-/* Acá va tu código */
+
+/*function cipher(){
+    var cajaCcipher = document.getElementById('cajaCipher');
+    var ccText = document.getElementById('ccText');
+    var cifradas = ''; 
+    for(var i = 0; i < ccText.length; i++){
+     
+      
+      var formula = (ccText.toUpperCase().charCodeAt(i) - 65 + 33) % 26 + 65;
+     
+       var cifrado = String.fromCharCode(formula);
+     
+    cifradas+= cifrado; 
+    }
+    cajaCcipher.innerHTML = cifradas; 
+ 
+    console.log(cifradas);
+ 
+}
+
+function decipher(){
+    var cajaDdecipher = document.getElementById('cajaDesipher');
+    var ddText = document.getElementById('ddText');
+    var dechiper = '';
+ 
+    for(var j = 0; j < propmt.length; j++){
+      
+        var formulaDechiper = (propmt.toUpperCase().charCodeAt(j) + 65 - 33) % 26 + 65;
+     
+      var decifradas = String.fromCharCode(formulaDechiper);
+        dechiper+= decifradas;
+    }
+    cajaDecipher.innerHTML = dechiper;
+}
