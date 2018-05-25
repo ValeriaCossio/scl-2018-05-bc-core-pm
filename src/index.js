d6669bb8d34d488a289ea45dd1cc32d7d6e58773
Valeria Cossio <valeriacossio5@gmail.com>
2018-05-25 18:24:35 -0400

    aa

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
 
-/*function cipher(){
-    var cajaCcipher = document.getElementById('cajaCipher');
-    var ccText = document.getElementById('ccText');
-    var cifradas = ''; 
-    for(var i = 0; i < ccText.length; i++){
+// function cipher(){
+//     var cajaCcipher = document.getElementById('cajaCipher');
+//     var ccText = document.getElementById('ccText');
+//     var cifradas = ''; 
+//     for(var i = 0; i < ccText.length; i++){
      
       
-      var formula = (ccText.toUpperCase().charCodeAt(i) - 65 + 33) % 26 + 65;
+//       var formula = (ccText.toUpperCase().charCodeAt(i) - 65 + 33) % 26 + 65;
      
-       var cifrado = String.fromCharCode(formula);
+//        var cifrado = String.fromCharCode(formula);
      
-    cifradas+= cifrado; 
-    }
-    cajaCcipher.innerHTML = cifradas; 
+//     cifradas+= cifrado; 
+//     }
+//     cajaCcipher.innerHTML = cifradas; 
  
-    console.log(cifradas);
+//     console.log(cifradas);
  
-}
+// }
 
-function decipher(){
-    var cajaDdecipher = document.getElementById('cajaDesipher');
-    var ddText = document.getElementById('ddText');
-    var dechiper = '';
+// function decipher(){
+//     var cajaDdecipher = document.getElementById('cajaDesipher');
+//     var ddText = document.getElementById('ddText');
+//     var dechiper = '';
  
-    for(var j = 0; j < propmt.length; j++){
+//     for(var j = 0; j < propmt.length; j++){
       
-        var formulaDechiper = (propmt.toUpperCase().charCodeAt(j) + 65 - 33) % 26 + 65;
+//         var formulaDechiper = (propmt.toUpperCase().charCodeAt(j) + 65 - 33) % 26 + 65;
      
-      var decifradas = String.fromCharCode(formulaDechiper);
-        dechiper+= decifradas;
-    }
-    cajaDecipher.innerHTML = dechiper;
-}
+//       var decifradas = String.fromCharCode(formulaDechiper);
+//         dechiper+= decifradas;
+//     }
+//     cajaDecipher.innerHTML = dechiper;
+// }
