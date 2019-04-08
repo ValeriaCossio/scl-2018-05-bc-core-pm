8f21c444775a99ea25b267c81ac66ceeec691f3d
valecj <valeriacossio5@gmail.com>
2019-04-08 01:12:37 -0400

    :ok_hand:

--- src/cipher.js
+++ src/cipher.js
@@ -1,28 +1,26 @@
 window.cipher = {
   encode: (string, offset) => {
-    string = document.getElementById('ccText').value;
-    offset = parseInt(document.getElementById('numerito').value);
+    string = document.getElementById('cipher-decipher').value;
+    offset = parseInt(document.getElementById('offset').value);
     let letrasCifradas = '';
     for (let i = 0; i < string.length; i++) {
       const cipher = (string.toUpperCase().charCodeAt(i) - 65 + offset) % 26 + 65;
       const move = String.fromCharCode(cipher);
       letrasCifradas += move;
     }
-    document.getElementById('cipherDecipher').innerHTML = letrasCifradas;
-    console.log("cifra ctm")
+    document.getElementById('result').innerHTML = letrasCifradas;
   },
   
   decode: (string, offset) => {
-    string = document.getElementById('ccText').value;
-    offset = parseInt(document.getElementById('numerito').value);
+    string = document.getElementById('cipher-decipher').value;
+    offset = parseInt(document.getElementById('offset').value);
     let letrasDescifradas = '';
     for (let o = 0; o < string.length; o++) {
       const cipher = (string.toUpperCase().charCodeAt(o) + 65 - offset) % 26 + 65;
       const move = String.fromCharCode(cipher);
       letrasDescifradas += move;
     }
-    document.getElementById('cipherDecipher').innerHTML = letrasDescifradas;
-    console.log("descifra ctm")
+    document.getElementById('result').innerHTML = letrasDescifradas;
   }
 };
 
